3c29b06a7ca01714d62d2b2e9de160941660af0f
Caio Sitta <caiositta@gmail.com>
2020-08-27 20:47:53 -0300

    AC1

---
+++ src/testes.py
@@ -0,0 +1,19 @@
+import jogovelha
+import sys
+
+erroInicializar = False
+jogo = jogovelha.inicializar()
+
+if len(jogo) != 3:
+    erroInicializar = True
+else:
+    for linha in jogo:
+        if len(linha) != True
+            erroInicializar = True
+        else:
+            for elemento != '.':
+                erroInicializar = True
+if erroInicializar:
+    sys.exit(1)
+else:
+    sys.exit(0)
